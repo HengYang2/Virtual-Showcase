e4609bf8b8ce92a851d570777b407f6f3f30f4ff
unknown <hengya03@gmail.com>
2024-01-12 10:44:18 -0600

    Encased textField elemnts in a FormControl component and then encased those in a form tag.

--- vite-project/src/components/LoginPage.jsx
+++ vite-project/src/components/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormControl, Button, TextField, Container, Stack } from '@mui/material';
+import { FormControl, Button, TextField, Container, Stack, FormLabel, FormGroup, FormControlLabel, Checkbox, Switch } from '@mui/material';
 
 export default function LoginForm() {
 
@@ -8,32 +8,62 @@ export default function LoginForm() {
         // Implement your login logic here
     };
 
+    // <Container onSubmit={handleSubmit} sx={{ height: '77%', width: '25%', padding: '', bgcolor: 'grey', position: 'absolute' }}>
+    // <FormControl sx={{ height: '20%', width: '80%', bgcolor: 'white' }}>
+    //     <TextField
+    //         label="Username"
+    //         variant="outlined"
+    //         type="text"
+    //         name="username"
+    //         required
+    //     />
+    // </FormControl>
+    // <FormControl sx={{ height: '77%', width: '25%' }}>
+    //     <TextField
+    //         label="Password"
+    //         variant="outlined"
+    //         type="password"
+    //         name="password"
+    //         required
+    //     />
+    // </FormControl>
+    // <Button type="submit" variant="contained" color="primary">
+    //     Login
+    // </Button>
+
+    // </Container>
     return (
-        <Container sx={{ height: '100%', minWidth: '100%', bgcolor: 'black', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: '0%', margin: '0%', }}>
+        <Container sx={{ height: '100%', minWidth: '100%', bgcolor: '', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: '0%', margin: '0%', }}>
             <img src='../../public/MrFrogBanner.png' />
-            <Container onSubmit={handleSubmit} sx={{ height: '77%', width: '25%', padding: '', bgcolor: 'grey', position: 'absolute' }}>
-                <FormControl sx={{ height: '20%', width: '80%', bgcolor: 'white' }}>
+            <form>
+                <FormControl sx={{ bgcolor: 'lightBlue', minWidth: '80%', }}>
                     <TextField
                         label="Username"
-                        variant="outlined"
-                        type="text"
-                        name="username"
+                        variant='outlined'
+                        type='text'
+                        name='username'
                         required
                     />
                 </FormControl>
-                <FormControl sx={{ height: '77%', width: '25%' }}>
+                <FormControl sx={{ bgcolor: 'lightBlue', minWidth: '80%', }}>
+                    <TextField
+                        label="Username"
+                        variant='outlined'
+                        type='text'
+                        name='username'
+                        required
+                    />
+                </FormControl>
+                <FormControl sx={{ bgcolor: 'lightBlue', minWidth: '80%', }}>
                     <TextField
                         label="Password"
-                        variant="outlined"
-                        type="password"
-                        name="password"
+                        variant='outlined'
+                        type='password'
+                        name='password'
                         required
                     />
                 </FormControl>
-                <Button type="submit" variant="contained" color="primary">
-                    Login
-                </Button>
-            </Container>
+            </form>
         </Container>
     );
 };
